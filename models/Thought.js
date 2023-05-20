8eb76822bfd441c9a972a88d5fe2cb2a72a6dc66
Maria Ciovacco Juarez <mariaciovaccojuarez@MBP-Maria.lan>
2023-05-19 23:42:54 -0500

    Adding models

---
+++ models/Thought.js
@@ -0,0 +1,34 @@
+const { Schema, model } = require('mongoose');
+const Reaction = require('./Reaction');
+
+
+const thoughtSchema = new Schema(
+  {
+    thoughtText: { type: String, required: true, minLength: 1, maxLength: 280 },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      // Use a getter method to format the timestamp on query
+    },
+    username: { type: String, required: true },
+    // user that created this thought
+    reactions: [Reaction],
+  },
+  {
+    toJSON: {
+      getters: true,
+    },
+    id: false,
+  }
+);
+
+thoughtSchema
+  .virtual('reactionCount')
+  .get(function () {
+    return this.reactions.length;
+  });
+
+// Initialize our Video model
+const Thought = model('Thought', thoughtSchema);
+
+module.exports = Thought;
---
+++ models/User.js
@@ -0,0 +1,40 @@
+const { Schema, model } = require('mongoose');
+
+// Schema to create User model
+
+const userSchema = new Schema(
+  {
+    username: {
+      type: String, 
+      required: true, 
+      unique: true,
+      trim: true
+    },
+    email: {
+      type: String, 
+      required: true, 
+      unique: true,
+      match: [/^\S+@\S+\.\S+$/, 'Please enter a valid email address'] 
+    },
+    thoughts: [{ type: Schema.Types.ObjectId, ref: 'Thought' }],
+    friends: [{ type: Schema.Types.ObjectId, ref: 'User' }]
+
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    id: false,
+  }
+);
+
+// Create a virtual called friendCount that retrieves the length of the user's friends array field on query.
+userSchema.virtual('friendCount').get(function() {
+  return this.friends.length;
+});
+
+
+// Initialize our User model
+const User = model('user', userSchema);
+
+module.exports = User;
