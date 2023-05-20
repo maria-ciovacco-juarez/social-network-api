8a8e6f6fa6a5dcc5328d0933a61c55ce76760a1f
Maria Ciovacco Juarez <mariaciovaccojuarez@MBP-Maria.lan>
2023-05-19 23:54:07 -0500

    syntax errors

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 const express = require('express');
-const routes = require('./routes';
-const db = require('./config/connection');
+const routes = require('./routes');
+const db = require('./config/connection')
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 // Middleware
-app.use(express.json(;
+app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(routes);
@@ -14,5 +14,5 @@ app.use(routes);
 db.once('open', () => {
   app.listen(PORT, () => {
     console.log(`Running on port ${PORT}!`);
-  })
-}
+  });
+});
