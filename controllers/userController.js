6fcd35c484bdbe0b9127f8dab68ec164c362f66c
Maria Ciovacco Juarez <mariaciovaccojuarez@MBP-Maria.lan>
2023-05-19 23:58:06 -0500

    fixed errors

--- controllers/userController.js
+++ controllers/userController.js
@@ -4,7 +4,7 @@ module.exports = {
   getUsers(req, res) {
     User.find()
       .then((users) => res.json(users))
-      .catch((err) => res.status(500).json(err))
+      .catch((err) => res.status(500).json(err));
   },
   getSingleUser(req, res) {
     User.findOne({ _id: req.params.userId })
@@ -14,12 +14,12 @@ module.exports = {
           ? res.status(404).json({ message: 'No user with that ID' })
           : res.json(user)
       )
-      .catch((err) => res.status(500).json(err))
+      .catch((err) => res.status(500).json(err));
   },
   // create a new user
   createUser(req, res) {
     User.create(req.body)
       .then((dbUserData) => res.json(dbUserData))
-      .catch((err) => res.status(500).json(err))
-  }
+      .catch((err) => res.status(500).json(err));
+  },
 };
