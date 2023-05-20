d33fb50bb5fcba8bb6cbfa6278116502395f4dcd
Maria Ciovacco Juarez <mariaciovaccojuarez@MBP-Maria.lan>
2023-05-19 23:45:23 -0500

    added routes

---
+++ routes/api/thoughtRoutes.js
@@ -0,0 +1,28 @@
+const router = require('express').Router();
+const {
+  getThought,
+  getSingleThought,
+  createThought,
+  updateThought,
+  deleteThought,
+  addThoughtResponse,
+  removeThoughtResponse,
+} = require('../../controllers/thoughtController');
+
+// /api/thought
+router.route('/').get(getThought).post(createThought);
+
+// /api/thought/:thoughtId
+router
+  .route('/:thoughtId')
+  .get(getSingleThought)
+  .put(updateThought)
+  .delete(deleteThought);
+
+// /api/thought/:thoughtId/reactions
+router.route('/:thoughtId/reactions').post(addThoughtResponse);
+
+// /api/thought/:thoughtId/reactions/:reactionId
+router.route('/:thoughtId/reactions/:reactionId').delete(removeThoughtResponse);
+
+module.exports = router;
